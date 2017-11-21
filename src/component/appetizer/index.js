f246e6cd0682e550413085a31b15644441b62022
mscharlock <michelle.scharlock@gmail.com>
2017-11-21 11:21:40 -0800

    Routes and basic text

---
+++ src/component/appetizer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Appetizer extends React.Component {
+  render() {
+    return (
+    <div className="appetizer">
+      <h3>Appetizer</h3>
+    </div>
+    );
+  }
+}
+
+export default Appetizer;
