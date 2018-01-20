fd3370410ba1a5a9811111a7c646ed36787900c6
Aaron McPeek <arn1313@comcast.net>
2018-01-20 14:06:03 -0800

    messing with photos

--- src/component/gallery/index.js
+++ src/component/gallery/index.js
@@ -4,15 +4,15 @@ class Gallery extends React.Component {
   render() {
     return (
       <div className="gallery">
-        <h3></h3>
-        <img src="./src/imgs/.png" alt="Photos of Dishes Availible"/>
+        <h3>Treats</h3>
+        <img src="src/imgs/TastyTreats.jpeg" alt="Photos of Dishes Availible"/>
 
         <h3></h3>
         <p></p>
-        <img src="./src/imgs/Soup.png"/>
+        // <img src="./src/imgs/Soup.png"/>
 
         <h3></h3>
-        <img src="./src/imgs/Salads.png"/>
+        // <img src="./src/imgs/Salads.png"/>
       </div>
     );
   }
